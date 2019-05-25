77b3722329b1f35735c88d1df92ba7319860560a
DONROB3R7 <costache.robert.valentin@gmail.com>
2019-05-25 02:47:42 +0200

    Carrousel
    Added an Carousel with infinite loop , autoplay.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Header from "../src/Components/Header_footer/header.js";
+import Featured from "./Components/featured";
 
 import "./styles.css";
 
@@ -13,6 +14,7 @@ class App extends Component {
     return (
       <div className="App" style={{ height: "1500px", background: "gray" }}>
         <Header />
+        <Featured />
       </div>
     );
   }
