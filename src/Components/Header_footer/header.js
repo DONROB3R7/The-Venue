bd209451b7bfbe291a7f02cc07a6acec89f7cd83
DONROB3R7 <costache.robert.valentin@gmail.com>
2019-05-25 02:17:55 +0200

    Scroll Effect
    Added scroll effect on the header

--- src/Components/Header_footer/header.js
+++ src/Components/Header_footer/header.js
@@ -9,9 +9,26 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drawerOpen: false
+      drawerOpen: false,
+      headerShow: false
     };
   }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.hanleScroll);
+  }
+
+  hanleScroll = () => {
+    if (window.scrollY > 0) {
+      this.setState({
+        headerShow: true
+      });
+    } else {
+      this.setState({
+        headerShow: false
+      });
+    }
+  };
 
   toggleDrawer = value => {
     this.setState({
@@ -24,7 +41,7 @@ class Header extends Component {
       <AppBar
         position="fixed"
         style={{
-          backgroundColor: "#2f2f2f",
+          backgroundColor: this.state.headerShow ? "#2f2f2f" : "transparent",
           boxShadow: "none",
           padding: "10px 0"
         }}
